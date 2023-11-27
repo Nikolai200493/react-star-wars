2f59d98826922b207609c842e9256bb3132bedde
Nikolay <Shaitor1993@mail.ru>
2023-11-27 15:59:01 +0300

    add deploy scripts

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -2,11 +2,12 @@ import styles from './App.module.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import routesConfig from '@routes/routesConfig'
 import Header from '@components/Header'
+import { REPO_NAME } from '@constants/repo'
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={`/${REPO_NAME}/`}>
         <div className={styles.wrapper}>
           <Header />
           <Routes>
